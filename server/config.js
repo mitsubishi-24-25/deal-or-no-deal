bd4bdef4f79e87f937bfc20907deb0fb3e430bb7
MITSUBISHI <clarencejefferson.bulatao.24@gmail.com>
2026-09-12 22:42:50 +0800

    Add files via upload

---
+++ server/config.js
@@ -0,0 +1,82 @@
+/*
+|--------------------------------------------------------------------------
+| GAME CONFIGURATION
+|--------------------------------------------------------------------------
+*/
+
+const config = {
+  /*
+  | Roulette names — palitan ayon sa actual student list.
+  */
+  rouletteNames: [
+    "Dereck",
+    "Francine",
+    "Yohann",
+    "Ryu",
+    "Ramessss",
+    "Prince",
+    "Jacob",
+    "Clarizze",
+    "Edrian",
+    "Clarence",
+    "Archie",
+    "Aron",
+    "Bil",
+    "Cristel",
+    "Sebastian",
+    "John",
+    "Genesis",
+    "Francis",
+    "Raiver",
+    "Emman",
+    "Rogelio",
+    "Chryz",
+    "Gian",
+    "Norvin",
+    "Ryza",
+    "Brayant",
+    "Aliyah",
+    "Krissa",
+    "Alliana",
+    "Geoffrey",
+    "James",
+    "Lorence",
+    "Ralph",
+    "Naericks",
+    "Princess",
+    "Princess",
+    "Rheian",
+    "Sharri",
+    "Sheena",
+    "Jheyron",
+    "Andrei",
+    "Aaron",
+    "Rhian",
+    "Justine"
+  ],
+
+  specificNameA: "Norvin",
+  specificNameB: "Bil",
+  specificNameC: "Yohann",
+
+  /*
+  | Group passwords — palitan mo sa gusto mong actual passwords.
+  | Note: Walang Group 3.
+  */
+    groupPasswords: {
+    1: "1234",
+    2: "5678",
+    4: "9012",
+    5: "3456",
+    6: "7890",
+    7: "2468",
+    8: "1357",
+    9: "6802"
+  },
+
+  // Teacher account (Ginoong Ricky)
+  teacherName: "Ginoong Ricky",
+  teacherPassword: "4816"
+};
+
+module.exports = config;
